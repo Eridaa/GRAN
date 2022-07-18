734d05ca5e4ba2d712bc3d490f948ad24644e69e
Eridaa <97359144+Eridaa@users.noreply.github.com>
2022-07-18 21:10:15 +0200

    adding cities in data_helper

--- utils/data_helper.py
+++ utils/data_helper.py
@@ -205,6 +205,15 @@ def create_graphs(graph_type, data_dir='data', noise=10.0, seed=1234):
         name='FIRSTMM_DB',
         node_attributes=False,
         graph_labels=True)
+  elif graph_type == 'Cities':
+    graphs = graph_load_batch(
+        data_dir,
+        min_num_nodes=0,
+        max_num_nodes=10000,
+        name='Cities',
+        node_attributes=False,
+        graph_labels=False)
+  
 
   num_nodes = [gg.number_of_nodes() for gg in graphs]
   num_edges = [gg.number_of_edges() for gg in graphs]
